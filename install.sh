13d562b8f2059edfe3fa144b147106db00e40262
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-04 14:10:27 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -151,12 +151,11 @@ questions3() {
         read -p "Please Enter your Config Port : " config_port
         read -p "Enter 'udp' for UDP connection (default is: tcp): " connection_type
         connection_type=${connection_type:-tcp}
-        argument="-L $connection_type://:$config_port  -F relay+wss://$foreign_ip:$port"
+        argument="-L $connection_type://:$config_port/127.0.0.1:$config_port -F relay+wss://$foreign_ip:$port"
         
     elif [ "$server_choice" == "2" ]; then
-        read -p "Please Enter your Config Port : " config_port
         read -p "Enter servers connection Port : " port
-        argument="-L relay+wss://:$port/:$config_port"
+        argument="-L relay+wss://:$port"
     else
         echo "Invalid choice. Please enter '1' or '2'."
         exit 1
