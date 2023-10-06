fe50ec33dcdd0eb2ebde9b5988ebeda91f4c43e9
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-10-06 14:03:08 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -334,6 +334,64 @@ install_quic() {
     get_inputs5
 }
 
+get_inputs6() {
+    read -p "Which server do you want to use? (Enter '1' for Iran[Internal] or '2' for Foreign[External]): " server_choice
+    if [ "$server_choice" == "1" ]; then
+        read -p "Enter foreign IP [External-ip]: " foreign_ip
+        read -p "Please enter the server's connection port: " port
+        read -p "Please enter your config port: " config_port
+        read -p "Enter your protocol (e.g : relay+wss) : " protocol
+        read -p "Enter 'udp' for UDP connection (default is: tcp): " connection_type
+        connection_type=${connection_type:-tcp}
+        argument="-L $connection_type://:$config_port/127.0.0.1:$config_port -F $protocol://$foreign_ip:$port"
+
+        read -p "Do you want to add more ports? (yes/no): " add_more_ports
+        while [ "$add_more_ports" == "yes" ]; do
+            read -p "Please enter additional config port(s) separated by commas (e.g., 2087,2095): " additional_config_ports
+            IFS=',' read -r -a ports_array <<< "$additional_config_ports"
+            for new_port in "${ports_array[@]}"; do
+                argument="-L $connection_type://:$new_port/127.0.0.1:$new_port $argument"
+            done
+            read -p "Do you want to add more ports? (yes/no): " add_more_ports
+        done
+
+    elif [ "$server_choice" == "2" ]; then
+        read -p "Enter server's connection port: " port
+        read -p "Enter your protocol (e.g : relay+wss) : " protocol
+        argument="-L $protocol://:$port"
+
+    else
+        echo "Invalid choice. Please enter '1' or '2'."
+        exit 1
+    fi
+
+    cd /etc/systemd/system
+
+    cat <<EOL>> gost.service
+[Unit]
+Description=GO Simple Tunnel
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/gost $argument
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable gost.service
+    sudo systemctl start gost.service
+}
+
+#install custom
+install_quic() {
+    install_gost
+    get_inputs6
+}
+
 #Uninstall 
 uninstall() {
     if ! command -v gost &> /dev/null
